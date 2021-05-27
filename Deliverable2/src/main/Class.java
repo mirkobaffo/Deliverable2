e8635368c2dd9add9626e28e0632f90d91da966d
mirko <mirkoleandri110392@gmail.com>
2021-05-27 12:12:45 +0200

    "Class"

---
+++ Deliverable2/src/main/Class.java
@@ -0,0 +1,41 @@
+package deliverable;
+
+public class Class {
+	
+	private String name;
+	private String IV;
+	private String OV;
+	private String FV;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String geIV() {
+		return this.IV;
+	}
+	
+	public String getOV() {
+		return this.OV;
+	}
+	
+	public String getFV() {
+		return this.FV;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+ 
+	public void setIV(String IV) {
+		this.IV = IV;
+	}
+	
+	public void setOV(String OV) {
+		this.OV = OV;
+	}
+	
+	public void setFV(String FV) {
+		this.FV = FV;
+	}
+}
